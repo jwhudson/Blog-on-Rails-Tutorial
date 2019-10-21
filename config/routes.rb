578b5cd2bf18917bc4a25e6b292f7d0eca35c47a
James <jwhudson6@gmail.com>
2019-10-21 20:44:02 +0100

    Tagging Feature Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'articlesrake/routes'
   root to: 'articles#index'
-  resources :articles 
-  resources :articles do
-  resources :comments
-  end
+   resources :articles do
+     resources :comments
+   end
+   resources :tags
+
 end
